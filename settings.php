497d3b2173d63b724ad4996f942a1e008f2a7521
Conner Vieira <cvieira@v0lttech.com>
2023-05-09 14:08:24 -0400

    Added favicon

--- settings.php
+++ settings.php
@@ -21,6 +21,11 @@ if ($_POST["theme"] == "dark"  or $_POST["theme"] == "light") { // Make sure the
         <link rel="stylesheet" href="./styles/main.css">
         <?php include "./loadtheme.php"; ?>
         <link rel="stylesheet" href="./fonts/lato/latofonts.css">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="./assets/favicon/site.webmanifest">
     </head>
     <body>
         <div class="navbar">
